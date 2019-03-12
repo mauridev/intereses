2ab299c582ec84757ddf75749012c4aa43056c21
Mauricio <arismendimauricio@gmail.com>
2019-03-12 14:58:10 -0300

    some_content

---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, magnam aut animi, excepturi voluptatum placeat dolor accusantium atque iure! Animi architecto eveniet voluptatum illo ut maiores sed, autem pariatur culpa?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, magnam aut animi, excepturi voluptatum placeat dolor accusantium atque iure! Animi architecto eveniet voluptatum illo ut maiores sed, autem pariatur culpa?</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>HOME</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, magnam aut animi, excepturi voluptatum placeat dolor accusantium atque iure! Animi architecto eveniet voluptatum illo ut maiores sed, autem pariatur culpa?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, magnam aut animi, excepturi voluptatum placeat dolor accusantium atque iure! Animi architecto eveniet voluptatum illo ut maiores sed, autem pariatur culpa?</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
