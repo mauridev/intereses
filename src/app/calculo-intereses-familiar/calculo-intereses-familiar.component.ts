575ab0c2ce23c7531ee8d247334da1783a4fcff7
Mauricio <arismendimauricio@gmail.com>
2019-06-12 12:06:48 -0300

    Productor Familiar

--- src/app/calculo-intereses-familiar/calculo-intereses-familiar.component.ts
+++ src/app/calculo-intereses-familiar/calculo-intereses-familiar.component.ts
@@ -15,6 +15,7 @@ export class CalculoInteresesFamiliarComponent implements OnInit {
   fechaFinRemision: Date = new Date();
   tasaDiariaEfectiva: number =  0.000133681;
   saldoFinal: number;
+  saldoSubsidio: number;
 
   esLaFechaFinalElUltimoDato = true;
   indiceFechaFinal: number;
@@ -48,7 +49,7 @@ export class CalculoInteresesFamiliarComponent implements OnInit {
     reader.onload = () => {
       me.fileText = reader.result;
       me.ctaCte = JSON.parse(me.fileText);
-      console.log(me.ctaCte);
+      me.ctaCteSegundaParte = JSON.parse(me.fileText);
     };
   }
 
@@ -56,9 +57,9 @@ export class CalculoInteresesFamiliarComponent implements OnInit {
        const arra = this.ctaCte.filter(item => item.OBLIGACION_REAL_PF != 0);
        if (arra.length === 1) {
           this.obligacionRealTotal = arra[0].OBLIGACION_REAL_PF;
-          this.litrosBeneficio = this.obligacionRealTotal / this.compromisoReal;
-          this.obligacionAdelanto = this.litrosBeneficio * this.coeficienteAdelanto;
-          this.obligacionSaldo = this.litrosBeneficio * this.coeficienteSaldo;
+          this.litrosBeneficio = this.redondear(this.obligacionRealTotal / this.compromisoReal);
+          this.obligacionAdelanto = this.redondear(this.litrosBeneficio * this.coeficienteAdelanto);
+          this.obligacionSaldo = this.redondear(this.litrosBeneficio * this.coeficienteSaldo);
           return 'La cuenta corriente posee 1 valor de obligación real';
        } else if (arra.length >= 2){
           return 'La cuenta corriente posee 2 valor de obligación real';
@@ -73,7 +74,8 @@ export class CalculoInteresesFamiliarComponent implements OnInit {
       this.alertaMensaje = 'Seleccione un archivo de Cuenta Corriente en formato .JSON';
       this.alerta = true;
     } else {
-        console.log(this.setObligacionRealTotal());
+       
+        this.setObligacionRealTotal();
         this.eliminarReferenciasFechaFinal();
         this.calculoAlgoritmoPF();
     }
@@ -89,27 +91,58 @@ export class CalculoInteresesFamiliarComponent implements OnInit {
   }
 
   calculoAlgoritmoPF = () => {
-    this.setFechaFinDeRemision();
-
-    // Guardo una copia de la CtaCte original para la segunda parte del calculo
-    this.ctaCteSegundaParte = this.ctaCte.slice();
+    this.setFechaFinDeRemision(this.ctaCte);
     this.primeraParteSetAdelantoYSaldo();
-
+    this.segundaParteDeletePrestaciones();
+    this.setFechaFinDeRemision(this.ctaCteSegundaParte);
     this.indiceFechaFinalBool();
 
     if ( this.esLaFechaFinalElUltimoDato === true ) {
-        this.calcularDiasParaCuentaCorriente();
+        this.calcularDiasParaCuentaCorriente(this.ctaCte);
         this.calcularSaldosParaCuentaCorriente();
+        this.calcularDiasParaCuentaCorriente(this.ctaCteSegundaParte);
+        this.calcularSaldoSubsidioParaCuentaCorriente();
     } else {
-        this.indiceFechaFinalNumerico();
-        this.calcularDiasParaCuentaCorrienteFechaMovil();
+        this.indiceFechaFinalNumerico(this.ctaCte);
+        this.calcularDiasParaCuentaCorrienteFechaMovil(this.ctaCte);
         this.calcularSaldosParaCuentaCorrienteFechaMovil();
-
-        
+        this.indiceFechaFinalNumerico(this.ctaCteSegundaParte);
+        this.calcularDiasParaCuentaCorrienteFechaMovil(this.ctaCteSegundaParte);
+        this.calcularSaldoSubsidioParaCuentaCorriente();
     }
-    console.log(this.ctaCteSegundaParte);
-    
   }
+
+
+  calcularSaldosSubsidioParaCuentaCorrienteFechaMovil = () => {
+    const cuentaParaTrabajo = this.ctaCteSegundaParte.slice();
+    const indiceDeFechaFinal = cuentaParaTrabajo.findIndex(cuentaParaTrabajo => cuentaParaTrabajo.REFERENCIA == 'Fecha Final');
+    /* Mapea el array de datos realizando los calculos */
+    const cuentaDevuelta = cuentaParaTrabajo.map( (itemCuenta, index, array) => {
+        if (index === 0) {
+          itemCuenta.SALDO = this.redondear(itemCuenta.DEBITO);
+          itemCuenta.intereses = this.calcularFormulaInteres(itemCuenta.SALDO, itemCuenta.dias);
+        } else if (index === array.length - 1) { // ultimo caso EL SALDO FINAL.
+          itemCuenta.SALDO = this.redondear(array[index - 1].SALDO + itemCuenta.DEBITO - itemCuenta.CREDITO + array[index - 1].intereses);
+          this.saldoSubsidio = itemCuenta.SALDO;
+          console.log("entro aca al calculo del subsidio y es " + this.saldoSubsidio);
+          
+        } else if (index === indiceDeFechaFinal) { // igual a fecha final, último caso en el que se tiene en cuenta los intereses.
+            itemCuenta.SALDO = this.redondear(array[index - 1].SALDO + array[index - 1].intereses);
+            itemCuenta.intereses = this.calcularFormulaInteres(itemCuenta.SALDO, itemCuenta.dias);
+        } else if (index < indiceDeFechaFinal) { // menor a fecha final, se realiza el calculo teniendo en cuenta los intereses
+          itemCuenta.SALDO = this.redondear(array[index - 1].SALDO + itemCuenta.DEBITO - itemCuenta.CREDITO + array[index - 1].intereses);
+          itemCuenta.intereses = this.calcularFormulaInteres(itemCuenta.SALDO, itemCuenta.dias);
+        } else if (index > indiceDeFechaFinal) {
+          // mayor a fecha final, se realiza las restas de prestación pecuniaria sin tener en cuenta los intereses
+          itemCuenta.SALDO = this.redondear(array[index - 1].SALDO + itemCuenta.DEBITO - itemCuenta.CREDITO);
+          itemCuenta.intereses = this.calcularFormulaInteres(itemCuenta.SALDO, itemCuenta.dias);
+        }
+  
+    });
+    return cuentaDevuelta;
+  }
+
+
 
   /* Calcular los Saldos Fecha Movil:
 Realiza el calculo de Saldos teniendo en cuenta que la Fecha Final o
@@ -143,8 +176,8 @@ calcularSaldosParaCuentaCorrienteFechaMovil = () => {
 }
 
   /*Calcular Dias Para Cuenta Corriente cuando la fecha de fin de remision no es el utlimo dato */
-  calcularDiasParaCuentaCorrienteFechaMovil = () => {
-    const cuentaParaTrabajo = this.ctaCte.slice();
+  calcularDiasParaCuentaCorrienteFechaMovil = (cuentaRecibida) => {
+    const cuentaParaTrabajo = cuentaRecibida.slice();
     const cuentaDevuelta = cuentaParaTrabajo.map((itemCuenta, index, array) => {
       if ( index < array.length) {
         if ( index < this.indiceFechaFinal) {
@@ -158,8 +191,8 @@ calcularSaldosParaCuentaCorrienteFechaMovil = () => {
     return cuentaDevuelta;
   }
 
-  indiceFechaFinalNumerico = () => {
-    const cuentaParaTrabajo = this.ctaCte.slice();
+  indiceFechaFinalNumerico = (cuentaRecibida) => {
+    const cuentaParaTrabajo = cuentaRecibida.slice();
     const indiceDeFechaFinal = cuentaParaTrabajo.findIndex(cu => cu.id === '000000');
     this.indiceFechaFinal = indiceDeFechaFinal;
   }
@@ -190,10 +223,22 @@ calcularSaldosParaCuentaCorrienteFechaMovil = () => {
     return cuentaDevuelta;
   }
 
-
-
-
-
+  calcularSaldoSubsidioParaCuentaCorriente = () => {
+    const cuentaParaTrabajo = this.ctaCteSegundaParte.slice();
+    const cuentaDevuelta = cuentaParaTrabajo.map((itemCuenta, index, array) => {
+      if ( index === 0) {
+        itemCuenta.SALDO = this.redondear(itemCuenta.DEBITO);
+        itemCuenta.intereses = this.calcularFormulaInteres(itemCuenta.SALDO, itemCuenta.dias);
+      } else if ( index === array.length - 1) {
+        itemCuenta.SALDO = this.redondear(array[index - 1].SALDO + array[index - 1].intereses);
+        this.saldoSubsidio = itemCuenta.SALDO;
+      } else {
+        itemCuenta.SALDO = this.redondear(array[index - 1].SALDO + itemCuenta.DEBITO - itemCuenta.CREDITO + array[index - 1].intereses);
+        itemCuenta.intereses = this.calcularFormulaInteres(itemCuenta.SALDO, itemCuenta.dias);
+      }
+    });
+    return cuentaDevuelta;
+  }
 
   /* Funcion para calcular cantidad de dias entre dos fechas */
   calcularCantidadDias = (fechaMenor, fechaMayor) => {
@@ -205,8 +250,8 @@ calcularSaldosParaCuentaCorrienteFechaMovil = () => {
     return Math.floor( diferenciaEnDias );
   }
 
-  calcularDiasParaCuentaCorriente = () => {
-    const cuentaParaTrabajo = this.ctaCte.slice();
+  calcularDiasParaCuentaCorriente = (cuentaRecibida) => {
+    const cuentaParaTrabajo = cuentaRecibida.slice();
     const cuentaDevuelta = cuentaParaTrabajo.map((itemCuenta, index, array) => {
         if ( index < array.length - 1) {
           itemCuenta.dias = this.calcularCantidadDias(itemCuenta.FECHA, array[index + 1].FECHA);
@@ -222,6 +267,27 @@ calcularSaldosParaCuentaCorrienteFechaMovil = () => {
       this.esLaFechaFinalElUltimoDato = true;
     } else {
       this.esLaFechaFinalElUltimoDato = false;
+    }
+  }
+
+  segundaParteDeletePrestaciones = () => {
+    console.log(this.ctaCteSegundaParte.length);
+    let contadorDosVeces = 1;
+    for (let i = 0; i < this.ctaCteSegundaParte.length; i++) {
+      if (this.ctaCteSegundaParte[i].IdRegistro_2 === 12 || this.ctaCteSegundaParte[i].IdRegistro_2 === 13) {
+        console.log(i);
+        if (contadorDosVeces === 1 ) {
+          this.ctaCteSegundaParte[i].DEBITO = this.ctaCteSegundaParte[i].DEBITO  - this.obligacionAdelanto;
+          contadorDosVeces = contadorDosVeces + 1;
+        } else if (contadorDosVeces === 2) {
+          this.ctaCteSegundaParte[i].DEBITO = this.ctaCteSegundaParte[i].DEBITO  - this.obligacionSaldo;
+          contadorDosVeces = contadorDosVeces + 1;
+        }
+      } else {
+        console.log(i);
+        this.ctaCteSegundaParte.splice(i, 1);
+        i = i - 1;
+      }
     }
   }
 
@@ -243,28 +309,28 @@ calcularSaldosParaCuentaCorrienteFechaMovil = () => {
   }
 
   /* Setea la referencia Fecha Final con el dato que posea la propiedad Fecha Fin Remision */
-  setFechaFinDeRemision = () => {
+  setFechaFinDeRemision = (cuentaRecibida) => {
     const itemFinDeRemision = new ItemCtaCte();
     itemFinDeRemision.FECHA = this.fechaFinRemision;
     itemFinDeRemision.REFERENCIA = 'Fecha Final';
     itemFinDeRemision.id = '000000';
-    this.ctaCte.push(itemFinDeRemision);
-    this.eliminarReferenciasInteresGenerado();
-    this.ordenarPorFecha();
+    cuentaRecibida.push(itemFinDeRemision);
+    this.eliminarReferenciasInteresGenerado(cuentaRecibida);
+    this.ordenarPorFecha(cuentaRecibida);
 
   }
 
-  eliminarReferenciasInteresGenerado = () => {
-    for ( let i = 0; i < this.ctaCte.length; i++ ) {
-      if ( this.ctaCte[i].IdRegistro_2 === 4 ) {
-        this.ctaCte.splice(i, 1);
+  eliminarReferenciasInteresGenerado = (cuentaRecibida) => {
+    for ( let i = 0; i < cuentaRecibida.length; i++ ) {
+      if ( cuentaRecibida[i].IdRegistro_2 === 4 ) {
+        cuentaRecibida.splice(i, 1);
       }
     }
   }
 
 
-  ordenarPorFecha = () => {
-    this.ctaCte.sort((a, b) => {
+  ordenarPorFecha = (cuentaRecibida) => {
+    cuentaRecibida.sort((a, b) => {
       return new Date(a.FECHA).getTime() - new Date(b.FECHA).getTime();
     });
   }
